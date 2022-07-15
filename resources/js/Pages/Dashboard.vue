6b79c8758799062b7e83ffbb58d137691288156e
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-16 00:06:46 +0900

    apiのテストをした

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,10 +1,5 @@
-<script setup>
-import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
-import { Head } from '@inertiajs/inertia-vue3';
-</script>
-
 <template>
-    <Head title="Dashboard" />
+    <Head title="Dashboard"/>
 
     <BreezeAuthenticatedLayout>
         <template #header>
@@ -19,8 +14,48 @@ import { Head } from '@inertiajs/inertia-vue3';
                     <div class="p-6 bg-white border-b border-gray-200">
                         You're logged in!
                     </div>
+                    <button @click="submit">push</button><br>
+                    <button @click="postSubmit">postSubmit</button><br>
+                    {{message}}
                 </div>
             </div>
+
         </div>
     </BreezeAuthenticatedLayout>
 </template>
+<!-- <script setup> -->
+
+<script>
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+export default {
+        data() {
+            return {
+                message:'aa'
+            }
+        },
+        components:{
+            Head,
+            BreezeAuthenticatedLayout
+        },
+        methods: {
+            async submit(){
+                await axios.get('api/test')
+                .then((res) =>{
+                    console.log(res);
+                    this.message = res.data.message
+                });
+            },
+            async postSubmit(){
+                await axios.post('api/postTest',{
+                    message:"post ok"
+                },{
+                    withCredentials: true
+                })
+                .then((res) =>{
+                    this.message = res.data.message
+                })
+            }
+        },
+    }
+</script>
