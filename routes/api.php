8ca737bcb4ac289df9b1c252ad9c9c4a8f0715ed
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-16 00:05:53 +0900

    とりあえずできた?

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->get('/test', function (Request $request) {
+    return response()->json([
+        "message" => "ok"
+    ]);
 });
+
+Route::middleware('auth:sanctum')->post('/postTest', function (Request $request) {
+    return response()->json([
+        "message" => $request->message
+    ]);
+});
+
+require __DIR__.'/auth.php';
