393815ab59c282be064abd9bed4c71f1cbcb57b5
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-16 00:04:27 +0900

    vuexした準備

---
+++ resources/js/store.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+// import axios from 'axios'
+
+export default createStore({
+	state: {
+	},
+	mutations: {
+
+    },
+    actions: {
+    },
+    modules: {
+    },
+    getters:{
+	}
+})
