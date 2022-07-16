424fa4f3e15024eeed664f337e429805f64986c3
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-16 23:40:45 +0900

    外部キーを別ファイルに書いた

---
+++ database/migrations/2022_07_16_140629_add_foreign_key_to_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            // 外部キー成約
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('article_tag_id')->references('id')->on('article_tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            // 外部キー削除
+            $table->dropForeign('articles_user_id_foreign');
+            $table->dropForeign('articles_article_tag_id_foreign');
+        });
+    }
+};
---
+++ database/migrations/2022_07_16_143443_add_foreign_key_to_article_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('article_tags', function (Blueprint $table) {
+            // 外部キー成約
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('tag_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('article_tags', function (Blueprint $table) {
+            // 外部キー削除
+            $table->dropForeign('article_tags_user_id_foreign');
+            $table->dropForeign('articles_tags_tag_id_foreign');
+        });
+    }
+};
