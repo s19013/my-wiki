7ace319c280ce8fc80311c039d12b2d015a4c956
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-16 18:15:45 +0900

    tagsマイグレーション作成

---
+++ database/migrations/2022_07_16_090655_create_tags_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->softDeletes();
+            $table->timestamps();
+
+            // 外部キー成約
+            $table->foreign('user_id')->references('id')->on('users');
+
+            //インデックス
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+};
