90b7c2427359059574d72268eaac4935a422fbc5
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-23 17:41:11 +0900

    表示させるダイアログだけ作った

---
+++ resources/js/Components/DeleteAlertComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-dialog
+      v-model="open"
+      persistent
+      max-width="290"
+    >
+      <v-card>
+        <v-card-title class="text-h5">
+          この記事を削除しますか
+        </v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="open = false"
+          >
+            もどる
+          </v-btn>
+          <v-btn
+            color="red darken-1"
+            text
+            @click="open = false"
+          >
+            削除する
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props:["open"]
+}
+</script>
