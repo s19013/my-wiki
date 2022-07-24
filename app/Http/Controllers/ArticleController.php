5e3edfd1ea0c6a5f63590d5eec869d2b5f4cf798
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-24 23:56:21 +0900

    まずは記事だけ保存できるようにした

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Tag;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
     //
     public function serveUserAllTag(Request $request)
     {
-        return Tag::getUserAllTag($request->id);
+        return Tag::getUserAllTag($request->userId);
+    }
+
+    public function store(Request $request)
+    {
+        Article::storeArticle(
+                userId   : $request->userId,
+                title    : $request->title,
+                body     : $request->article,
+                category : $request->category,
+            );
     }
 }
