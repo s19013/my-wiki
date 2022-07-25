3b8f03e963f2af4d07db31541306d17884459728
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-25 23:45:53 +0900

    新規記事についているタグを保存

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Tag;
+use App\Models\ArticleTag;
 use App\Models\Article;
 
 class ArticleController extends Controller
@@ -16,11 +17,31 @@ class ArticleController extends Controller
 
     public function store(Request $request)
     {
-        Article::storeArticle(
+        // 記事を保存して記事のidを取得
+        $articleId = Article::storeArticle(
                 userId   : $request->userId,
-                title    : $request->title,
-                body     : $request->article,
+                title    : $request->articleTitle,
+                body     : $request->articleBody,
                 category : $request->category,
+        );
+
+        // なんのタグも設定されていない時
+        if (empty($request->tagList) == true) {
+            ArticleTag::storeArticleTag(
+                userId    : $request->userId,
+                tagId     : null,
+                articleId : $articleId,
             );
+        }
+        //タグが設定されている時
+        else {
+            foreach($request->tagList as $tagId){
+                ArticleTag::storeArticleTag(
+                    userId    : $request->userId,
+                    tagId     : $tagId,
+                    articleId : $articleId,
+                );
+            }
+        }
     }
 }
--- app/Models/ArticleTag.php
+++ app/Models/ArticleTag.php
@@ -5,11 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use DB;
+
 class ArticleTag extends Model
 {
     use HasFactory;
     protected $fillable = [
+        'article_id',
         'user_id',
         'tag_id',
     ];
+
+    public static function storeArticleTag($tagId,$articleId,$userId)
+    {
+        DB::transaction(function () use($tagId,$articleId,$userId){
+            ArticleTag::create([
+                'article_id' => $articleId,
+                'user_id'    => $userId,
+                'tag_id'     => $tagId,
+            ]);
+        });
+    }
 }
