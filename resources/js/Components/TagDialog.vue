67e7aad85e610b73ce07a151cce5b49cbeed39b9
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-27 12:31:25 +0900

    一旦保存

---
+++ resources/js/Components/TagDialog.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-dialog
+        v-model="open"
+        persistent
+        scrollable
+    >
+    <v-card>
+        <v-card-title class="text-h5">タグ</v-card-title>
+        <ul>
+            <li></li>
+        </ul>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            新規作成
+          </v-btn>
+        </v-card-actions>
+    </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default{
+
+}
+</script>
