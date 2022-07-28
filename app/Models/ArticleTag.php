578de7008bec557bcf2e59b97edac5ff913df4ae
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-29 00:22:47 +0900

    メモ書き

--- app/Models/ArticleTag.php
+++ app/Models/ArticleTag.php
@@ -30,6 +30,7 @@ class ArticleTag extends Model
     public static function serveAricleTag($articleId)
     {
         // tagsターブルとくっつける
+        // article_tags.tag_id = tags.id
         // タグ自体消されていないタグと記事からはずされていないタグを取得
         return ArticleTag::select('tags.name')
         ->leftJoin('tags','article_tags.tag_id','=','tags.id')
