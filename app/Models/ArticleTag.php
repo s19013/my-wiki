e5d83c4cf376e66bc263a52cfe8adebd505d8166
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-30 22:11:00 +0900

    アップデートに関する関数と削除に関する関数を作った

--- app/Models/ArticleTag.php
+++ app/Models/ArticleTag.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use DB;
+use Carbon\Carbon;
 
 class ArticleTag extends Model
 {
@@ -27,6 +28,58 @@ class ArticleTag extends Model
         });
     }
 
+    public static function deleteArticleTag($tagId,$articleId)
+    {
+        DB::transaction(function () use($tagId,$articleId){
+            ArticleTag::where('article_id','=',$articleId)
+            ->where('tag_id','=',$tagId)
+            ->update(['deleted_at' => date(Carbon::now())]);
+        });
+    }
+
+    public static function updateAricleTag($articleId,$updatedTagList)
+    {
+
+        // 消された､追加されたを確認する
+        $originalTagList = [];
+        // $deletedTagList  = [];
+        // $addedTagList    = [];
+
+        // もとのタグを確認する
+        $original = ArticleTag::select('tag_id')
+        ->where('article_id','=',$articleId)
+        ->get();
+
+        foreach ($original as $tag){
+            array_push($originalTagList,$tag->original["tag_id"]);
+        }
+
+        // 追加されたタグ
+        $addedTagList = array_diff($updatedTagList, $originalTagList);
+        // 削除されたタグ
+        $deletedTagList = array_diff($originalTagList, $updatedTagList);
+
+        //追加
+        if (!empty($addedTagList)) {
+            foreach($addedTagList as $tag) {
+                ArticleTag::storeArticleTag(
+                    tagId:$tag,
+                    articleId:$articleId,
+                    userId:\Auth::id()
+                );
+            }
+        }
+
+        //削除
+        if (!empty($deletedTagList)) {
+            foreach($deletedTagList as $tag) {
+                ArticleTag::deleteArticleTag(
+                    tagId:$tag,
+                    articleId:$articleId,
+                );
+            }
+        }
+    }
 
     //記事に関連付けられたタグを取得
     public static function serveTagsRelatedToAricle($articleId,$userId)
