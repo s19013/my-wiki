a9754c6a1a8b6026bb92c46a70e53b5a69ec9f2e
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-31 17:24:18 +0900

    tag用のコントローラーを作った

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    //
+    public function tagStore(Request $request)
+    {
+        // CSRFトークンを再生成して、二重送信対策
+        $request->session()->regenerateToken();
+        return Tag::store(
+            userId:Auth::id(),
+            tag   :$request->tag,
+        );
+    }
+
+    public function tagSearch(Request $request)
+    {
+        return Tag::search(
+            userId:Auth::id(),
+            tag   :$request->tag
+        );
+    }
+}
