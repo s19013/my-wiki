558aa0539bad831be640dfa9bf2fa377fc286ada
知念英弥 <s19013@std.it-college.ac.jp>
2022-07-31 17:24:46 +0900

    tagに関する処理を別のコントローラーに移した

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -71,24 +71,6 @@ class ArticleController extends Controller
         return Article::serveUserAllArticle(userId:Auth::id());
     }
 
-    public function tagStore(Request $request)
-    {
-        // CSRFトークンを再生成して、二重送信対策
-        $request->session()->regenerateToken();
-        return Tag::store(
-            userId:Auth::id(),
-            tag   :$request->tag,
-        );
-    }
-
-    public function tagSearch(Request $request)
-    {
-        return Tag::search(
-            userId:Auth::id(),
-            tag   :$request->tag
-        );
-    }
-
     // 編集か新規かを分ける
     // public function DetermineProcessing(Request $request)
     // {
