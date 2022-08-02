a77268c740c924712421d6e2503ef882fb5dcbe9
知念英弥 <s19013@std.it-college.ac.jp>
2022-08-02 23:51:24 +0900

    ブックマーク編集画面へ遷移するための関数を作った

--- app/Http/Controllers/TransitionController.php
+++ app/Http/Controllers/TransitionController.php
@@ -6,9 +6,13 @@ use Illuminate\Http\Request;
 
 
 use Inertia\Inertia;
+
 use App\Models\Tag;
 use App\Models\ArticleTag;
 use App\Models\Article;
+use App\Models\BookMark;
+use App\Models\BookMarkTag;
+
 use Auth;
 
 class TransitionController extends Controller
@@ -57,6 +61,30 @@ class TransitionController extends Controller
         return Inertia::render('Article/EditArticle',[
             'originalArticle'     => $article,
             'originalCheckedTag' => $articleTag,
+        ]);
+    }
+
+    public function transitionToEditBookMark($bookMarkId)
+    {
+        //削除された記事ならindexに戻す
+        $isDeleted = BookMark::checkBookMarkDeleted(bookMarkId:$bookMarkId);
+        if ($isDeleted == true ) { return redirect()->route('SearchBookMark'); }
+
+        // 他人の記事を覗こうとしているならindexに戻す
+        $isSamePerson = BookMark::preventPeep(bookMarkId:$bookMarkId,userId:Auth::id());
+        if ($isSamePerson == false) { return redirect()->route('SearchBookMark'); }
+
+        $bookMark = BookMark::serveBookMark(bookMarkId:$bookMarkId);
+
+        $bookMarkTag = BookMarkTag::serveTagsRelatedToAricle(
+            userId:Auth::id(),
+            bookMarkId:$bookMarkId
+        );
+
+
+        return Inertia::render('BookMark/EditBookMark',[
+            'originalBookMark'     => $bookMark,
+            'originalCheckedTag'   => $bookMarkTag,
         ]);
     }
 }
