f5928022434f910794b9cb45131b37977c831749
知念英弥 <s19013@std.it-college.ac.jp>
2022-08-08 16:33:18 +0900

    Revert "これで本番環境でapiが動くと思う"
    This reverts commit 165f9986308bac8b9d9056e1f3327667654c2a24.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,6 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.withCredentials = true;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
