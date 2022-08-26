a320b2884c75326a9dc45ef39f04ee1468232e14
知念英弥 <s19013@std.it-college.ac.jp>
2022-08-27 00:32:38 +0900

    画面の大きさによってボタンの大きさを変える必要があるためボタンを個別のコンポーネント化する

---
+++ resources/js/Components/DeleteButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <v-btn color="error" @click.stop="click">
+            <v-icon>mdi-trash-can</v-icon>
+            削除
+        </v-btn>
+    </div>
+</template>
+
+<script>
+export default{
+    methods: {
+        click(){this.$emit('click');}
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@media (max-width: 600px){
+    .v-btn{
+        height: 80%;
+        width : 80%;
+    }
+}
+</style>
---
+++ resources/js/Components/SaveButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="saveButton">
+        <v-btn color="#BBDEFB" @click.stop="click()" :disabled="disabled">
+            <v-icon>mdi-content-save</v-icon>
+            保存
+        </v-btn>
+    </div>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+        }
+    },
+    methods: {
+        click(){this.$emit('click');}
+    },
+    props:['disabled'],
+}
+</script>
+
+<style lang="scss" scoped>
+@media (max-width: 600px){
+    .v-btn{
+        height: 80%;
+        width : 80%;
+    }
+}
+</style>
