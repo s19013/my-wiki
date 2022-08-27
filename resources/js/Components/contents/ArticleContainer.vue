cc45a1550e3e6113be0d75c332345010ac566c3d
知念英弥 <s19013@std.it-college.ac.jp>
2022-08-28 01:06:13 +0900

    コンポーネント化した

---
+++ resources/js/Components/contents/ArticleContainer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class ="articleContainer" v-show="!loading">
+        <h2>{{title}}</h2>
+        <Link :href="'/Article/View/' + id">
+            <v-btn color="submit" elevation="2">
+                閲覧 編集
+            </v-btn>
+        </Link>
+    </div>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue3';
+export default{
+    components:{Link},
+    props:['title','id','loading']
+}
+</script>
+
+<style scoped lang="scss">
+.articleContainer{
+    display: grid;
+    grid-template-columns:10fr 1fr;
+    background-color: #e1e1e1;
+    border:black solid 1px;
+    margin-bottom:20px;
+    padding: 5px;
+    button{width: 100%;}
+    h2{grid-column: 1/2;}
+    a{
+        text-decoration: none;
+        color: black;
+    }
+}
+
+</style>
