770eaf64fd265f7e6b48a72a7a46b3e21c8a5649
知念英弥 <s19013@std.it-college.ac.jp>
2022-08-27 14:51:04 +0900

    sectionタグからdivタグに変更

--- resources/js/Pages/Article/ViewArticle.vue
+++ resources/js/Pages/Article/ViewArticle.vue
@@ -1,12 +1,12 @@
 <template>
     <BaseLayout :title="'my-wiki ' + article.title" pageTitle="記事観覧">
-        <section class="articleContainer">
+        <div class="articleContainer">
                 <!-- タイトルとボタン2つ -->
                 <v-row class="head">
                     <v-col cols="10">
                         <h1>{{article.title}}</h1>
                     </v-col>
-                    <v-col cols="1"> <DeleteAlertComponent @deleteAricleTrigger="deleteArticle"></DeleteAlertComponent> </v-col>
+                    <v-col cols="1"> <DeleteAlertComponent @deleteTrigger="deleteArticle"></DeleteAlertComponent> </v-col>
                     <v-col cols="1">
                         <Link :href="'/Article/Edit/' + article.id">
                             <v-btn class="longButton" color="#BBDEFB">
@@ -22,7 +22,7 @@
 
                 <!-- タブ -->
                 <TagList :tagList="articleTagList"/>
-        </section>
+        </div>
         <loadingDialog :loadingFlag="articleDeleting"></loadingDialog>
     </BaseLayout>
 </template>
@@ -74,12 +74,6 @@ export default{
 
 <style lang="scss">
 .articleContainer {margin: 0 20px;}
-textarea {
-    width: 100%;
-    resize: none;
-    background-color: #f6f6f6;
-    padding: 20px;
-}
 .markdown{
     margin-top:20px;
     margin-bottom:30px;
@@ -88,28 +82,6 @@ textarea {
     overflow-wrap:normal;
 }
 
-.tab{
-    ul{
-        display: flex;
-        flex-wrap: wrap;
-    }
-    li{
-        list-style:none;
-        border:black solid 1px;
-        padding: 0 10px ;
-        margin:5px;
-        cursor: default;
-
-    }
-}
-
 .head{margin-top: 10px;}
-.articleError{padding-top: 5px;}
-.v-input__details{
-    margin: 0;
-    padding: 0;
-    height: 0;
-    width: 0;
-}
 
 </style>
