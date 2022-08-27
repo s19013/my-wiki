ec50f32db208048bb236eecb6191951f2487f708
知念英弥 <s19013@std.it-college.ac.jp>
2022-08-27 15:01:23 +0900

    `tyep`propsを受け取ってテキストを変更できるようにした

--- resources/js/Components/dialog/DeleteAlertDialog.vue
+++ resources/js/Components/dialog/DeleteAlertDialog.vue
@@ -8,7 +8,7 @@
       persistent
     >
         <section class="Dialog">
-            <h2>この記事を削除しますか</h2>
+            <h2>{{text}}</h2>
             <v-row>
                 <v-col cols=""></v-col>
                 <v-col cols="2">
@@ -29,6 +29,13 @@ export default {
     data() {
         return {
             deleteDialogFlag:false,
+            text:null,
+        }
+    },
+    props:{
+        type:{
+            type   :String,
+            default:"article"
         }
     },
     components:{
@@ -39,6 +46,11 @@ export default {
         deleteDialogFlagSwitch(){this.deleteDialogFlag = !this.deleteDialogFlag},
         //ダイアログ内の削除するボタンを押したことを親に伝える
         deleteTrigger(){this.$emit("deleteTrigger");}
+    },
+    mounted() {
+        //textの切り替え
+        if(this.type.toLowerCase() == 'article'){ this.text = "この記事を削除しますか" }
+        else {this.text = "このブックマークを削除しますか"}
     },
 }
 </script>
