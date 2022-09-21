a9fd5c84815785462e268ca0639c367a686d9a42
知念英弥 <s19013@std.it-college.ac.jp>
2022-09-21 13:19:00 +0900

    ファクトリーを作った

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\Article;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    protected $model = Article::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title'=>fake()->realText(15),
+            'body' =>fake()->realText(50),
+            'user_id' => rand (1, 3),
+        ];
+    }
+}
