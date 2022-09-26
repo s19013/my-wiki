657bfc35297319e8c99af245da0fc47ccb2626cc
知念英弥 <s19013@std.it-college.ac.jp>
2022-09-26 20:22:49 +0900

    bookmarkのファクトリー生成

---
+++ database/factories/BookMarkFactory copy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\BookMark;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BookMark>
+ */
+class BookMarkFactory extends Factory
+{
+    protected $model = BookMark::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title'=>fake()->realText(15),
+            'url' =>fake()->realText(30),
+            'user_id' => rand (1, 2),
+        ];
+    }
+}
---
+++ database/factories/BookMarkTagFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BookMarkTag>
+ */
+class BookMarkTagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'book_mark_id' => rand (1, 2),
+            'tag_id' => rand (1, 10)
+        ];
+    }
+}
