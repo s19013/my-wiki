174711dcfb073df999dd0ee20839909c5f1009b8
知念英弥 <s19013@std.it-college.ac.jp>
2022-09-28 18:56:05 +0900

    作成日､更新日は四角の外に配置することにした

--- resources/js/Components/contents/ArticleContainer.vue
+++ resources/js/Components/contents/ArticleContainer.vue
@@ -1,12 +1,14 @@
 <template>
     <div class ="content">
-        <h2>{{article.title}}</h2>
         <DateLabel :createdAt="article.created_at" :updatedAt="article.updated_at" size="0.1rem"/>
-        <Link :href="'/Article/View/' + article.id">
-            <v-btn color="submit" elevation="2">
-                閲覧 編集
-            </v-btn>
-        </Link>
+        <div class="elements">
+            <h2>{{article.title}}</h2>
+            <Link :href="'/Article/View/' + article.id">
+                <v-btn color="submit" elevation="2">
+                    閲覧 編集
+                </v-btn>
+            </Link>
+        </div>
     </div>
 </template>
 
@@ -22,25 +24,21 @@ export default{
 }
 </script>
 
-<style scoped lang="scss">
-.content{
+<style scope lang="scss">
+.DateLabel{ justify-content: flex-end; }
+.elements{
     display: grid;
-    grid-template-rows:1fr,10fr;
     grid-template-columns:10fr 1fr;
+    gap:0.5rem;
     background-color: #e1e1e1;
     border:black solid 1px;
     padding: 5px;
     button{width: 100%;}
-    .DateLabel{
-        grid-row: 1/2;
-        grid-column: 1/3;
-    }
     h2{
-        grid-row: 2/3;
+        margin: auto 0;
         grid-column: 1/2;
     }
     a{
-        grid-row: 2/3;
         grid-column: 2/3;
         text-decoration: none;
         color: black;
--- resources/js/Components/contents/BookMarkContainer.vue
+++ resources/js/Components/contents/BookMarkContainer.vue
@@ -2,17 +2,19 @@
     <div class ="content">
         <!-- 別タブで開くようにする -->
         <DateLabel :createdAt="bookMark.created_at" :updatedAt="bookMark.updated_at" size="0.1rem"/>
-        <a :href="url" target="_blank" rel="noopener noreferrer">
-            <h2>
-                <v-icon>mdi-arrow-top-left-bold-box-outline</v-icon>
-                {{bookMark.title}}
-            </h2>
-        </a>
-        <Link :href="'/BookMark/Edit/' + bookMark.id">
-            <v-btn color="submit" elevation="2">
-                編集
-            </v-btn>
-        </Link>
+        <div class="elements">
+            <a :href="url" target="_blank" rel="noopener noreferrer">
+                <h2>
+                    <v-icon>mdi-arrow-top-left-bold-box-outline</v-icon>
+                    {{bookMark.title}}
+                </h2>
+            </a>
+            <Link :href="'/BookMark/Edit/' + bookMark.id">
+                <v-btn color="submit" elevation="2">
+                    編集
+                </v-btn>
+            </Link>
+        </div>
     </div>
 </template>
 
@@ -31,26 +33,24 @@ export default{
 </script>
 
 <style scoped lang="scss">
-.content{
+.DateLabel{ justify-content: flex-end; }
+.elements{
     display: grid;
-    grid-template-rows:1fr,10fr;
     grid-template-columns:10fr 1fr;
+    gap:0.5rem;
     background-color: #e1e1e1;
     border:black solid 1px;
     margin-bottom:20px;
     padding: 5px;
-    button{width: 100%;}
-    .DateLabel{
-        grid-row: 1/2;
-        grid-column: 1/3;
+    button{
+        width: 100%;
+        grid-column: 2/3;
     }
     h2{
-        grid-row: 2/3;
+        margin: auto 0;
         grid-column: 1/2;
     }
     a{
-        grid-row: 2/3;
-        grid-column: 2/3;
         text-decoration: none;
         color: black;
     }
