d9638a38ef098e1352f9724ecbaa41ad7181a95a
知念英弥 <s19013@std.it-college.ac.jp>
2022-10-01 00:04:57 +0900

    updateとdeleteの処理追加

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -20,20 +20,54 @@ class TagController extends Controller
             tag   :$request->tag,
         );
 
-        // すでに登録していた
-        if ($result == false) {
+        // 登録できた
+        if ($result) {
             return response()->json(
-                ["message" => "already exists"],
-                400
+                ["message" => "stored"],
+                200
             );
         }
 
-        // 登録できた
+        // すでに登録していた
         return response()->json(
-            ["message" => "stored"],
-            200
+            ["message" => "already exists"],
+            400
         );
     }
+
+    public function tagUpdate(Request $request)
+    {
+        // CSRFトークンを再生成して、二重送信対策
+        $request->session()->regenerateToken();
+
+        $result = Tag::updateTag(
+            userId:Auth::id(),
+            tagId :$request->id,
+            name  :$request->name
+        );
+
+
+        // 更新できた
+        if ($result) {
+            return response()->json(
+                ["message" => "updated"],
+                200
+            );
+        }
+
+        // 既に登録している名前と被った
+        return response()->json(
+            ["message" => "already exists"],
+            400
+        );
+    }
+
+    public function tagDelete()
+    {
+        Tag::delete(tagId :$request->id);
+    }
+
+
 
     //タグ検索
     public function tagSearch(Request $request)
