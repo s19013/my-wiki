463ef216a0859b2ebad286bb951e9d7c9313d4cb
知念英弥 <s19013@std.it-college.ac.jp>
2022-11-06 23:26:35 +0900

    articleだけを作るSeederを作った

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Article;
+use App\Models\ArticleTag;
+use App\Models\Tag;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $mainArticles = Article::factory()->count(10)->create(['user_id' => 13]);
+        foreach ($mainArticles as $article){
+            ArticleTag::factory()->create([
+                    "article_id" => $article->id,
+                    Tag::factory()->create()->id
+            ]);
+        }
+    }
+}
