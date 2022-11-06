3c292386d0c93e00065a123d48490f8b536ccf8d
知念英弥 <s19013@std.it-college.ac.jp>
2022-11-06 23:28:20 +0900

    このファイルは別ファイルのseederを呼び出すだけにする

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,46 +22,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $mainUser  = User::factory()->create([
-            'email'    => 'hideya670@gmail.com',
-            'password' => Hash::make("gemini0522"),
-        ]);
-        $otherUser = User::factory()->create();
-        // $mainTags      = Tag::factory()->create();
-
-        $mainArticles = Article::factory()->count(10)->create(['user_id' => $mainUser->id]);
-        $otherArticles = Article::factory()->count(10)->create(['user_id' => $otherUser->id]);
-
-        foreach ($mainArticles as $article){
-            ArticleTag::factory()->create([
-                "article_id" => $article->id,
-                Tag::factory()->create()->id
-            ]);
-        }
-
-        foreach ($otherArticles as $article){
-            ArticleTag::factory()->create([
-                "article_id" => $article->id,
-                Tag::factory()->create()->id
-            ]);
-        }
-
-        $mainBookMarks = BookMark::factory()->count(10)->create(['user_id' => $mainUser->id]);
-        $otherBookMarks = BookMark::factory()->count(10)->create(['user_id' => $otherUser->id]);
-
-        foreach ($mainBookMarks as $bookMark){
-            BookMarkTag::factory()->create([
-                "book_mark_id" => $bookMark->id,
-                Tag::factory()->create()->id
-            ]);
-        }
-
-        foreach ($otherBookMarks as $bookMark){
-            BookMarkTag::factory()->create([
-                "book_mark_id" => $bookMark->id,
-                Tag::factory()->create()->id
-            ]);
-        }
-
+        $this->call(ArticleSeeder::class);
     }
 }
