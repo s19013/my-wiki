2dd77e538c48e81808b257b3bfea7b07e458a48e
知念英弥 <s19013@std.it-college.ac.jp>
2022-11-17 20:18:32 +0900

    バリデーションエラーが起きた時のjsonを返すようにした

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -5,7 +5,8 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-use Illuminate\Validation\Validator;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ArticleRequest extends FormRequest
 {
@@ -33,6 +34,16 @@ class ArticleRequest extends FormRequest
         return [
             "articleTitle.max"    => "126文字以内で入力してください",
         ];
+    }
+
+
+    protected function failedValidation(Validator $validator)
+    {
+        $res = response()->json([
+            'errors' => $validator->errors(),
+            ],
+            400);
+        throw new HttpResponseException($res);
     }
 }
 
