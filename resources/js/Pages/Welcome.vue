ea5ade4ce6d11289646a802c898cc272817ed30c
知念英弥 <s19013@std.it-college.ac.jp>
2022-11-23 20:11:22 +0900

    作品名を変えたので編集

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -31,7 +31,7 @@ defineProps({
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-10 sm:justify-start sm:pt-0">
-                <h1>My-Wiki</h1>
+                <h1>Sundlf</h1>
             </div>
 
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
