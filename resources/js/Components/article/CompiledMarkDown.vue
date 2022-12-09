45458f3d6d8c4339c054cae629bcda3d7af631a4
知念英弥 <s19013@std.it-college.ac.jp>
2022-12-09 12:34:02 +0900

    github-markdown-cssを使った

--- resources/js/Components/article/CompiledMarkDown.vue
+++ resources/js/Components/article/CompiledMarkDown.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="CompiledMarkDown" v-html="compileMarkDown()"></div>
+    <div class="CompiledMarkDown markdown-body" v-html="compileMarkDown()"></div>
 </template>
 
 <script>
 import {marked} from 'marked';
+import githubMarkdownCss from 'github-markdown-css';
 import sanitizeHtml from 'sanitize-html';
 
 // エンターで改行できるように設定
@@ -27,36 +28,36 @@ export default {
 </script>
 
 <!-- なぜかわからないがscopedをつけたら中のh1タグなどが反応しない -->
-<style lang="scss">
+<style lang="scss" scoped>
 .CompiledMarkDown{
     padding-bottom: 2rem;
     word-break   :break-word;
     overflow-wrap:normal;
     list-style-position:inside;
-    h1,h2,h3,h4,h5{ margin:0.5rem 0; }
-    ul,ol{ margin:0.5rem 0; }
-    th,td {
-        padding:0.2rem;
-        border: solid 1px;
-    }
-    p{
-        code{
-            background-color: #e5e5e5;
-            color: #000000;
-            padding: 0 0.3rem;
-            margin: 0 0.3rem;
-        }
-    }
-    pre{
-        background-color: #364549;
-        color: #e3e3e3;
-        padding: 0.8rem;
-        // スクロールバー表示
-        overflow: auto;
-    }
-    table {
-        border-collapse:  collapse; /* セルの線を重ねる */
-        margin:0.5rem 0;
-    }
+//     h1,h2,h3,h4,h5{ margin:0.5rem 0; }
+//     ul,ol{ margin:0.5rem 0; }
+//     th,td {
+//         padding:0.2rem;
+//         border: solid 1px;
+//     }
+//     p{
+//         code{
+//             background-color: #e5e5e5;
+//             color: #000000;
+//             padding: 0 0.3rem;
+//             margin: 0 0.3rem;
+//         }
+//     }
+//     pre{
+//         background-color: #364549;
+//         color: #e3e3e3;
+//         padding: 0.8rem;
+//         // スクロールバー表示
+//         overflow: auto;
+//     }
+//     table {
+//         border-collapse:  collapse; /* セルの線を重ねる */
+//         margin:0.5rem 0;
+//     }
 }
 </style>
