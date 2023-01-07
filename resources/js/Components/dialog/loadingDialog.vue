22bbd281df27cf1433ac10c870dd31e07aeee58c
知念英弥 <s19013@std.it-college.ac.jp>
2023-01-07 10:56:04 +0900

    ダイアログだと閉じないと画面遷移した時にスクロールができなくなる｡ ユーザーが操作できなくすればよいので､z-indexなどで作りなおした

--- resources/js/Components/dialog/loadingDialog.vue
+++ resources/js/Components/dialog/loadingDialog.vue
@@ -1,16 +1,14 @@
 <template>
-    <div>
-        <v-dialog
-            v-model="flag"
-            persistent
-        >
-            <loading></loading>
-        </v-dialog>
+    <div v-show="loadingFlag">
+        <v-progress-circular
+            :size=100
+            color="primary"
+            indeterminate
+        ></v-progress-circular>
     </div>
 </template>
 
 <script>
-import loading from '@/Components/loading/loading.vue'
 export default{
     data() {
         return {
@@ -18,15 +16,35 @@ export default{
         }
     },
     props:['loadingFlag'],
-    components:{loading},
     watch: {
         // propsをv-modelに渡しても､propsが変化した時にv-modelの値が変わらないらしい
         // なのでwatchを使って､コンポーネント内の変数を変化させる方法にした
         loadingFlag: function(newValue, oldValue){
             this.flag = !this.flag
-            console.log(this.flag);
         }
     }
 }
 
 </script>
+
+<style scoped lang="scss">
+div{
+    background: #bdbdbdc8;
+    z-index:20;
+    width:100%;
+    height: 100%;
+    position: fixed;
+    top:0px;
+    left:0px;
+    .v-progress-circular{
+        position: absolute;
+        // スマホだと割りとずれるのがきになる
+        top:45%;
+        left:46%;
+        margin:auto;
+    }
+}
+// どうにかして画面中央に置かねば positionで行けたはず
+// 画面目一杯に表示しないと行けない
+</style>
+
