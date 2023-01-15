898174f2a8fff923e4f0f63666e53f14e984d8fa
知念英弥 <s19013@std.it-college.ac.jp>
2023-01-16 00:53:08 +0900

    コメント類追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+
+        {{-- レスポンシブデザインに必要らしい --}}
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
@@ -23,7 +25,10 @@
         {{-- google広告 --}}
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6551381289717615" crossorigin="anonymous"></script>
         {{-- seo対策 --}}
+        {{-- googleに見つけてもらう --}}
         <meta name="google-site-verification" content="ero83E0pFy6Cmszzqy2IoKzSt-oFYzsg6hqXfemz0MM" />
+        {{-- 説明文 --}}
+        <meta name="description" content="メモやブックマークをタグをつけて保存する" />
     </head>
     <style>
         /* #app {
