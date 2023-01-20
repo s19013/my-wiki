3475fc470b6034d1d3badf2260f7523b43052277
知念英弥 <s19013@std.it-college.ac.jp>
2023-01-20 20:57:46 +0900

    スマホなどでは縦に並べる 英語､日本語でurlを変える

--- resources/js/Components/foot/originalFooter.vue
+++ resources/js/Components/foot/originalFooter.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="originalFooter">
-        <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/e/2PACX-1vQRfqPmWcI2irs1HpRBOjA9lyo2CiIFWRBpWY2lmHnMM8gWZUEmng57BEs1t-VC5Bd_kSCHhmG9gmAA/pub">{{ messages.terms }}</a>
-        <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/e/2PACX-1vQCW5pRoXeXHiZJ-vz8MImLVm-XTViLIdy1TxTBtsbAAzYb4MpPEaEFucHaWnpzDkI905s5AeW6rui3/pub">{{ messages.privacy }}</a>
-        <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/UMU57uzKa1r1E4zBA">{{ messages.enquiry }}</a>
+        <a target="_blank" rel="noopener noreferrer" :href="messages.termsUrl">{{ messages.terms }}</a>
+        <a target="_blank" rel="noopener noreferrer" :href="messages.privacyUrl">{{ messages.privacy }}</a>
+        <a target="_blank" rel="noopener noreferrer" :href="messages.enquiryUrl">{{ messages.enquiry }}</a>
 
         <v-dialog v-model="deleteDialogFlag">
             <template v-slot:activator="{ props }">
@@ -36,8 +36,11 @@ export default{
         return {
             japanese:{
                 terms:"利用規約",
+                termsUrl:"https://docs.google.com/document/d/e/2PACX-1vQRfqPmWcI2irs1HpRBOjA9lyo2CiIFWRBpWY2lmHnMM8gWZUEmng57BEs1t-VC5Bd_kSCHhmG9gmAA/pub",
                 privacy:"プライバシーポリシー",
+                privacyUrl:"https://docs.google.com/document/d/e/2PACX-1vQCW5pRoXeXHiZJ-vz8MImLVm-XTViLIdy1TxTBtsbAAzYb4MpPEaEFucHaWnpzDkI905s5AeW6rui3/pub",
                 enquiry:"問い合わせ",
+                enquiryUrl:"https://forms.gle/9tXeZcXmqH2zR6SP9",
                 withdrawal:"退会",
                 caution:"本当に退会しますか",
                 message:"メモ､ブックマーク､タグは削除されます(復元できません)",
@@ -45,8 +48,11 @@ export default{
             },
             messages:{
                 terms:"terms of service",
+                termsUrl:"https://docs.google.com/document/d/e/2PACX-1vSdKoL_dJ3sVwsBBdNh-lVetKD-WupAVq56CYTpTkpkcMHvTNfBruWBnqleHuYDPa7yv1CkdfCL79im/pub",
                 privacy:"privacy policy",
+                privacyUrl:"https://docs.google.com/document/d/e/2PACX-1vQ0WcXsD3kbYli0SqzaQ0yks_r5uuIgIClMLmT_4g6WLbx9zjcXeoAvxHcLmxbQC_OkrFcieFUaghU_/pub",
                 enquiry:"enquiry",
+                enquiryUrl:"https://forms.gle/Y4RPh3k3V9G3wnNp9",
                 withdrawal:"withdrawal",
                 caution:"do you really want to leave",
                 message:"All registered data will be deleted (cannot be restored).",
@@ -68,13 +74,16 @@ export default{
 
 <style lang="scss" scoped>
 .originalFooter{
+    padding:1rem 0;
     background-color: rgb(127, 255, 174);
-    height        : 3rem;
     display: flex;
     justify-content: center;
     align-items: center;
     gap: 1rem;
     a {color: black;}
+    @media (max-width: 960px){
+        flex-flow: column;
+    }
 }
 </style>
 
