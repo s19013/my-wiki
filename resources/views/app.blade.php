575dd9fdb196da73c01a7b997953db959ffeec92
知念英弥 <s19013@std.it-college.ac.jp>
2023-01-23 10:59:07 +0900

    承認されなかったのでしばらくコメントアウトしておく

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,7 +23,7 @@
         @inertiaHead
 
         {{-- google広告 --}}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6551381289717615" crossorigin="anonymous"></script>
+        {{-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6551381289717615" crossorigin="anonymous"></script> --}}
         {{-- seo対策 --}}
         {{-- googleに見つけてもらう --}}
         <meta name="google-site-verification" content="ero83E0pFy6Cmszzqy2IoKzSt-oFYzsg6hqXfemz0MM" />
