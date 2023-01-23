1341459ca899c7164ffbab71754b8f6ad825d1cd
知念英弥 <s19013@std.it-college.ac.jp>
2023-01-23 11:36:29 +0900

    テスト用のページを編集

--- resources/js/Pages/test.vue
+++ resources/js/Pages/test.vue
@@ -1,68 +1,26 @@
 <template>
-    <div id="editor">
-        <h1>aaa</h1>
-        <textarea :value="article" @input="update"></textarea>
-        <div v-html="compiledMarkdown"></div>
+    <div>
+        <SearchField></SearchField>
+        <p>aaa</p>
     </div>
 </template>
 
 <script>
-import {marked} from 'marked'
+
+import SearchField from '@/Components/SearchField.vue';
 export default {
     data() {
       return {
-        activeTab:0,
-        title:null,
-        article: '# hello'
+
       }
     },
-    computed: {
-        compiledMarkdown: function() {
-            return marked(this.article);
-        }
-    },
-    methods: {
-      update(e) {this.article = e.target.value;},
+    components:{
+      SearchField,
     }
 }
 </script>
 
 <style>
-body{
-    all: initial;
-}
-html,
-body,
-#editor {
-  margin: 0;
-  height: 100%;
-  font-family: "Helvetica Neue", Arial, sans-serif;
-  color: #333;
-}
 
-textarea,
-#editor div {
-  display: inline-block;
-  width: 49%;
-  height: 100%;
-  vertical-align: top;
-  box-sizing: border-box;
-  padding: 0 20px;
-}
-
-textarea {
-  border: none;
-  border-right: 1px solid #ccc;
-  resize: none;
-  outline: none;
-  background-color: #f6f6f6;
-  font-size: 14px;
-  font-family: "Monaco", courier, monospace;
-  padding: 20px;
-}
-
-code {
-  color: #f66;
-}
 
 </style>
