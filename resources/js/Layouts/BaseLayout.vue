c8488f566e1919a7c25d5dbd59600ccde80fc9e4
知念英弥 <s19013@std.it-college.ac.jp>
2023-01-24 17:08:41 +0900

    広告を貼り付けた

--- resources/js/Layouts/BaseLayout.vue
+++ resources/js/Layouts/BaseLayout.vue
@@ -7,26 +7,38 @@
         <v-main>
             <slot />
         </v-main>
+        <BarAd></BarAd>
         <originalFooter/>
     </v-app>
 
 </template>
 
 <script>
-    import { Head } from '@inertiajs/inertia-vue3'
-    import originalHead from '@/Components/head/originalHead.vue'
-    import originalFooter from '@/Components/foot/originalFooter.vue'
+import { Head } from '@inertiajs/inertia-vue3'
+import originalHead from '@/Components/head/originalHead.vue'
+import originalFooter from '@/Components/foot/originalFooter.vue'
+import BarAd from '@/Components/Ads/BarAd.vue';
 
-    export default {
-        components: {
-            Head,
-            originalHead,
-            originalFooter
-        },
-        props:['title','pageTitle'],
-        mounted(){
-            // 言語拾う
-            if ((window.navigator.language).substring(0,2) == "ja") {this.$store.state.lang = "ja"}
-        }
+export default {
+    components: {
+        Head,
+        originalHead,
+        originalFooter,
+        BarAd,
+    },
+    props:['title','pageTitle'],
+    mounted(){
+        // 言語拾う
+        if ((window.navigator.language).substring(0,2) == "ja") {this.$store.state.lang = "ja"}
     }
+}
 </script>
+
+<style lang="scss" scoped>
+#ad {
+    margin: 1rem 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
