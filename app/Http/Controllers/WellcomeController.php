9feb037ef024e0bce252257787c55e6c473f2832
知念英弥 <s19013@std.it-college.ac.jp>
2023-01-25 22:46:02 +0900

    toolkitを編集したことによる修正

--- app/Http/Controllers/WellcomeController.php
+++ app/Http/Controllers/WellcomeController.php
@@ -15,14 +15,17 @@ class WellcomeController extends Controller
 {
     public function returnJa()
     {
+        MetaToolKit::setUrl('https://sundlf.com/ja');
+
+        MetaToolKit::setImg('https://sundlf.com/sundlf_logo_og.png');
+
+        MetaToolKit::setMeta('og:locale','ja_JP');
+
+        MetaToolKit::setTitleTag("sundlf  -- タグを使ってメモ､ブックマークを整理");
+
         MetaToolKit::setTitle("sundlf  -- タグを使ってメモ､ブックマークを整理");
 
-        MetaToolKit::setNameMeta('twitter:title','sundlf  -- タグを使ってメモ､ブックマークを整理');
-        MetaToolKit::setNameMeta('description','メモ､ブックマークにタグを付けて保存して整理､検索などで探しやすくするツールアプリです');
-        MetaToolKit::setNameMeta('twitter:description','メモ､ブックマークにタグを付けて保存して整理､検索などで探しやすくするツールアプリです');
-
-        MetaToolKit::setPropertyMeta('og:title','sundlf  -- タグを使ってメモ､ブックマークを整理');
-        MetaToolKit::setPropertyMeta('og:description','メモ､ブックマークにタグを付けて保存して整理､検索などで探しやすくするツールアプリです');
+        MetaToolKit::setDescription("メモ､ブックマークにタグを付けて保存して整理､検索などで探しやすくするツールアプリです");
 
         return Inertia::render('Welcome', [
             'canLogin' => Route::has('login'),
@@ -35,14 +38,17 @@ class WellcomeController extends Controller
 
     public function returnEn()
     {
+        MetaToolKit::setUrl('https://sundlf.com/en');
+
+        MetaToolKit::setImg('https://sundlf.com/sundlf_logo_og.png');
+
+        MetaToolKit::setMeta('og:locale','en_US');
+
+        MetaToolKit::setTitleTag("sundlf  -- Organize article and bookmarks using tags");
+
         MetaToolKit::setTitle("sundlf  -- Organize article and bookmarks using tags");
 
-        MetaToolKit::setNameMeta('twitter:title','sundlf  -- Organize article and bookmarks using tags');
-        MetaToolKit::setNameMeta('description','this application is makes it easier to find by adding tags to memos and bookmarks, saving them, organizing them, and searching them.');
-        MetaToolKit::setNameMeta('twitter:description','this application is makes it easier to find by adding tags to memos and bookmarks, saving them, organizing them, and searching them.');
-
-        MetaToolKit::setPropertyMeta('og:title','sundlf  -- Organize article and bookmarks using tags');
-        MetaToolKit::setPropertyMeta('og:description','this application is makes it easier to find by adding tags to memos and bookmarks, saving them, organizing them, and searching them.');
+        MetaToolKit::setDescription("description','this application is makes it easier to find by adding tags to memos and bookmarks, saving them, organizing them, and searching them.");
 
         return Inertia::render('Welcome', [
             'canLogin' => Route::has('login'),
