76ba53ce42bc4724cbfaa941604d93cfd27774a6
知念英弥 <s19013@std.it-college.ac.jp>
2023-02-07 16:48:39 +0900

    metaタグに関して実験

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -49,8 +49,10 @@ onMounted(() => {
 <template>
     <v-container>
         <Head>
-            <!-- <title>{{ messages.title }}</title>
-            <meta name="description" :content="messages.message" /> -->
+            <title>{{ messages.title }}</title>
+            <meta name="description" :content="messages.message" />
+            <meta inertia property="og:title" :content="messages.title"/>
+            <meta inertia property="og:description" :content="messages.message"/>
             <link rel="alternate" hreflang="ja" href="https://sundlf.com/">
             <link rel="alternate" hreflang="en" href="https://sundlf.com/en/">
         </Head>
