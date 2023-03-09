1874988d2a3d9b958bd0ee03f5673c39ae2c87b2
知念英弥 <s19013@std.it-college.ac.jp>
2023-03-09 22:34:24 +0900

    デバック用の値が入っていたので修正

--- resources/js/Components/SearchOption.vue
+++ resources/js/Components/SearchOption.vue
@@ -6,7 +6,7 @@
             ref="searchQuantity"
             :selected="oldSearchQuantity"
             :label="messages.searchQuantity"
-            :list="[2,5,10,20,30,40,50]"
+            :list="[10,20,30,40,50]"
          />
 
          <SelectComponent
