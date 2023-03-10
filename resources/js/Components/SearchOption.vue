72c572702df094330432206f723b1327bcb5d6b7
知念英弥 <s19013@std.it-college.ac.jp>
2023-03-10 15:23:03 +0900

    selectOptionのselected引数をオブジェクト型にしたのでその編集

--- resources/js/Components/SearchOption.vue
+++ resources/js/Components/SearchOption.vue
@@ -6,7 +6,7 @@
             ref="searchQuantity"
             :selected="oldSearchQuantity"
             :label="messages.searchQuantity"
-            :list="[10,20,30,40,50]"
+            :list="searchQuantityLabelList"
          />
 
          <SelectComponent
@@ -14,7 +14,7 @@
             ref="sort"
             :selected="sortSelected"
             :label="messages.sortLabel"
-            :list="messages.sort"
+            :list="sortLabelList"
             :object="true"
 
          />
@@ -29,89 +29,22 @@ export default{
             japanese:{
                 searchQuantity:"検索数",
                 sortLabel:"並び順",
-                sort:[
-                    {
-                        label:"更新日 新 → 古",
-                        value:"updated_at_desc"
-                    },
-                    {
-                        label:"更新日 古 → 新",
-                        value:"updated_at_asc"
-                    },
-                    {
-                        label:"作成日 新 → 古",
-                        value:"created_at_desc"
-                    },
-                    {
-                        label:"作成日 古 → 新",
-                        value:"created_at_asc"
-                    },
-                    {
-                        label:"タイトル あ → ん",
-                        value:"title_asc"
-                    },
-                    {
-                        label:"タイトル ん → あ",
-                        value:"title_desc"
-                    },
-                    {
-                        label:"閲覧数 多 → 少",
-                        value:"count_desc"
-                    },
-                    {
-                        label:"閲覧数 少 → 多",
-                        value:"count_asc"
-                    },
-                    {
-                        label:"ランダム",
-                        value:"random"
-                    },
-                ]
             },
             messages:{
                 searchQuantity:"search quantity",
                 sortLabel:"sort",
-                sort:[
-                    {
-                        label:"Updated Date new → old",
-                        value:"updated_at_desc"
-                    },
-                    {
-                        label:"Updated Date old → new",
-                        value:"updated_at_asc"
-                    },
-                    {
-                        label:"Created Date new → old",
-                        value:"created_at_desc"
-                    },
-                    {
-                        label:"Created Date old → new",
-                        value:"created_at_asc"
-                    },
-                    {
-                        label:"Title A → Z",
-                        value:"title_asc"
-                    },
-                    {
-                        label:"Title Z → A",
-                        value:"title_desc"
-                    },
-                    {
-                        label:"Views Most → Less",
-                        value:"count_desc"
-                    },
-                    {
-                        label:"Views Less → Most",
-                        value:"count_asc"
-                    },
-                    {
-                        label:"Random",
-                        value:"random"
-                    },
-                ]
             },
-            searchQuantity:this.oldSearchQuantity, //後でoldにする
-            sortSelected:{label:"updated desc",value:"updated_at_desc"},
+            searchQuantity:this.oldSearchQuantity,
+            searchQuantityLabelList:[
+                {label:"2",value:2},
+                {label:"10",value:10},
+                {label:"20",value:20},
+                {label:"30",value:30},
+                {label:"40",value:40},
+                {label:"50",value:50},
+            ],
+            sortSelected:this.oldSortType,
+
         }
     },
     components:{SelectComponent},
@@ -123,24 +56,19 @@ export default{
         oldSortType:{
             type:String,
             default:"updated_at_desc"
+        },
+        sortLabelList:{
+            type: Array,
+            default:[]
         }
     },
     methods: {
-        serveSearchQuantity(){return this.$refs.searchQuantity.serveSelected()},
-        serveSort(){return (this.$refs.sort.serveSelected()).value},
-        setSort(){
-            // 引数はvalueだけだが{label:,value:}の形でないとmodelが反応しないため{label:,value:}にして返す
-            return this.messages.sort.find(element => element.value == this.oldSortType);
-        }
+        serveSearchQuantity(){return this.$refs.searchQuantity.serveLocalSelected()},
+        serveSort(){return (this.$refs.sort.serveLocalSelected())},
     },
     mounted() {
         this.$nextTick(function () {
             if (this.$store.state.lang == "ja"){Object.assign(this.messages,this.japanese)}
-            this.sortSelected = this.setSort()
-
-            // タイミングの問題でpropsからではなくsetterで初期値を入れる
-            this.$refs.searchQuantity.setSelected(this.oldSearchQuantity)
-            this.$refs.sort.setSelected(this.sortSelected)
         })
     },
 }
