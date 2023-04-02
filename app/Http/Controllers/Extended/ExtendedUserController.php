14e53c883921ac7e9b333d9b01b950515c4d90e0
知念英弥 <s19013@std.it-college.ac.jp>
2023-04-02 23:02:29 +0900

    バリデーションをリクエストファイルに任せた｡ユーザーが存在するか確認した

--- app/Http/Controllers/Extended/ExtendedUserController.php
+++ app/Http/Controllers/Extended/ExtendedUserController.php
@@ -7,35 +7,33 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Validation\ValidationException;
 use \Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Hash;
+
+use App\Http\Requests\ExtendedLoginRequest;
 
 use App\Models\User;
 
 class ExtendedUserController extends Controller
 {
-    public function login(Request $request)
+    public function login(ExtendedLoginRequest $request)
     {
-        // credentials:認証情報
-        $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
+        // ユーザーが存在するか確認
+        $isUserExists = User::where('email','=',$request->email)
+        ->where('password','=',Hash::make($request->password))
+        ->exists();
 
-        if (Auth::attempt($credentials)) {
-            $user = User::whereEmail($request->email)->first();
+        if (!$isUserExists) {return response()->json('User Not Found.', Response::HTTP_INTERNAL_SERVER_ERROR);}
 
-            $user->tokens()->delete();
-            $token = $user->createToken("login:user{$user->id}")->plainTextToken;
-
-            return response()->json(['token' => $token ], Response::HTTP_OK);
-        }
-
-        return response()->json('User Not Found.', Response::HTTP_INTERNAL_SERVER_ERROR);
+        $user = User::whereEmail($request->email)->first();
+        $user->tokens()->delete();
+        $token = $user->createToken("login:user{$user->id}")->plainTextToken;
+        return response()->json(['token' => $token ], Response::HTTP_OK);
     }
 
     public function logout()
     {
         Auth::guard('sanctum')->user()->tokens()->delete();
-        $res = [];
+        $res =[message => 'see you'];
         return response()->json($res, Response::HTTP_OK);
     }
 }
