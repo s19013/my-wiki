5516b4c6b9b974d85d616d68686e00249e8a8133
知念英弥 <s19013@std.it-college.ac.jp>
2023-11-11 22:41:35 +0900

    フォーマッター有効化

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -1,36 +1,44 @@
 <script setup>
-import BreezeApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/inertia-vue3';
-import { ref, onMounted, onUnmounted,nextTick } from 'vue';
+import BreezeApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/inertia-vue3";
+import { ref, onMounted, onUnmounted, nextTick } from "vue";
 import { useStore } from "vuex";
-import originalFooter from '@/Components/foot/originalFooter.vue'
+import originalFooter from "@/Components/foot/originalFooter.vue";
 
-const store = useStore()
+const store = useStore();
 
 onMounted(() => {
-    if ((window.navigator.language).substring(0,2) == "ja") {store.commit('setLang','ja')}
-})
+    if (window.navigator.language.substring(0, 2) == "ja") {
+        store.commit("setLang", "ja");
+    }
+});
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div
+        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"
+    >
         <div>
             <Link href="/">
-                <img :src="'/sundlf_logo.png'" alt="ロゴ"/>
+                <img :src="'/sundlf_logo.png'" alt="ロゴ" />
             </Link>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div
+            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+        >
             <slot />
         </div>
     </div>
-    <originalFooter/>
+    <originalFooter />
 </template>
 
 <style lang="scss" scoped>
 img {
-    width:30vw;
-    @media (max-width: 900px){width:50vw;}
+    width: 30vw;
+    @media (max-width: 900px) {
+        width: 50vw;
+    }
     height: auto;
 }
 </style>
