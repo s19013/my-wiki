b05871ad3d38302ce2295afa29e7063518901250
知念英弥 <s19013@std.it-college.ac.jp>
2023-11-12 23:02:27 +0900

    describeが使えるようなので修正

--- e2e/login/login.spec.js
+++ e2e/login/login.spec.js
@@ -21,81 +21,84 @@ test('default', async ({ page }) => {
     await expect(page.getByRole('button', { name: 'ログイン' })).toBeVisible();
 })
 
-// 画面遷移
-// 条件:新規登録ボタンを押す
-// 期待:新規登録画面が表示される
-test('画面遷移:新規登録画面',async({page}) => {
-    await page.goto(baseHome + 'login');
-    await page.getByRole('link', { name: '新規登録' }).click();
-    await expect(page).toHaveURL(baseHome + "register");
+test.describe("画面遷移",() => {
+    // 条件:新規登録ボタンを押す
+    // 期待:新規登録画面が表示される
+    test('新規登録画面',async({page}) => {
+        await page.goto(baseHome + 'login');
+        await page.getByRole('link', { name: '新規登録' }).click();
+        await expect(page).toHaveURL(baseHome + "register");
+    })
+
+    // 条件:｢パスワードをわすれましたか?｣ボタンを押す
+    // 期待:パスワード再発行画面が表示される
+    test('パスワード再発行画面',async({page}) => {
+        await page.goto(baseHome + 'login');
+        await page.getByRole('link', { name: 'パスワードをわすれましたか?' }).click();
+        await expect(page).toHaveURL(baseHome + "forgot-password");
+    })
 })
 
-// 条件:｢パスワードをわすれましたか?｣ボタンを押す
-// 期待:パスワード再発行画面が表示される
-test('画面遷移:パスワード再発行画面',async({page}) => {
-    await page.goto(baseHome + 'login');
-    await page.getByRole('link', { name: 'パスワードをわすれましたか?' }).click();
-    await expect(page).toHaveURL(baseHome + "forgot-password");
-})
+test.describe('ログイン失敗',() => {
+    // 条件:何も埋めてない
+    // 期待:｢このフィールドを入力してください｣と表示される
+    test('何も埋めてない',async({page}) => {
+        await page.goto(baseHome + 'login');
+        await page.getByRole('button', { name: 'ログイン' }).click();
+        await page.screenshot({ path: 'playwright-screenshot/login/ログイン失敗-何も埋めてない-jp.jpg', fullPage: false });
+    })
 
-// ログイン失敗
-// 条件:何も埋めてない
-// 期待:｢このフィールドを入力してください｣と表示される
-test('ログイン失敗:何も埋めてない',async({page}) => {
-    await page.goto(baseHome + 'login');
-    await page.getByRole('button', { name: 'ログイン' }).click();
-    await page.screenshot({ path: 'playwright-screenshot/login/ログイン失敗-何も埋めてない-jp.jpg', fullPage: false });
-})
+    // 条件:メアドの欄に適当な文字列を入れる
+    // 期待:｢[@]を挿入してください｣と表示される
+    test('メアド欄にメアド以外の文字列を入力',async({page}) => {
+        await page.goto(baseHome + 'login');
+        await page.getByLabel('メールアドレス').fill('aaa');
+        await page.getByRole('button', { name: 'ログイン' }).click();
+        await page.screenshot({ path: 'playwright-screenshot/login/ログイン失敗-メアド欄にメアド以外の文字列を入力-jp.jpg', fullPage: false });
+    })
 
-// 条件:メアドの欄に適当な文字列を入れる
-// 期待:｢[@]を挿入してください｣と表示される
-test('ログイン失敗:メアド欄にメアド以外の文字列を入力',async({page}) => {
-    await page.goto(baseHome + 'login');
-    await page.getByLabel('メールアドレス').fill('aaa');
-    await page.getByRole('button', { name: 'ログイン' }).click();
-    await page.screenshot({ path: 'playwright-screenshot/login/ログイン失敗-メアド欄にメアド以外の文字列を入力-jp.jpg', fullPage: false });
-})
+    // 条件:メアドだけ埋めた
+    // 期待:｢このフィールドを入力してください｣と表示される
+    test('メアドだけ埋めた',async({page}) => {
+        await page.goto(baseHome + 'login');
+        await page.getByLabel('メールアドレス').fill(testmail);
+        await page.getByRole('button', { name: 'ログイン' }).click();
+        await page.screenshot({ path: 'playwright-screenshot/login/ログイン失敗-メアドだけ埋めた-jp.jpg', fullPage: false });
+    })
 
-// 条件:メアドだけ埋めた
-// 期待:｢このフィールドを入力してください｣と表示される
-test('ログイン失敗:メアドだけ埋めた',async({page}) => {
-    await page.goto(baseHome + 'login');
-    await page.getByLabel('メールアドレス').fill(testmail);
-    await page.getByRole('button', { name: 'ログイン' }).click();
-    await page.screenshot({ path: 'playwright-screenshot/login/ログイン失敗-メアドだけ埋めた-jp.jpg', fullPage: false });
-})
+    // 条件:パスワードだけ埋めた
+    // 期待:｢このフィールドを入力してください｣と表示される
+    test('パスワードだけ埋めた',async({page}) => {
+        await page.goto(baseHome + 'login');
+        await page.getByLabel('パスワード').fill(testpass);
+        await page.getByRole('button', { name: 'ログイン' }).click();
+        await page.screenshot({ path: 'playwright-screenshot/login/ログイン失敗-パスワードだけ埋めた-jp.jpg', fullPage: false });
+    })
 
-// 条件:パスワードだけ埋めた
-// 期待:｢このフィールドを入力してください｣と表示される
-test('ログイン失敗:パスワードだけ埋めた',async({page}) => {
-    await page.goto(baseHome + 'login');
-    await page.getByLabel('パスワード').fill(testpass);
-    await page.getByRole('button', { name: 'ログイン' }).click();
-    await page.screenshot({ path: 'playwright-screenshot/login/ログイン失敗-パスワードだけ埋めた-jp.jpg', fullPage: false });
-})
+    // 条件:パスワードが間違ってる
+    // 期待:｢このフィールドを入力してください｣と表示される
+    test('パスワードが間違ってる',async({page}) => {
+        await page.goto(baseHome + 'login');
+        await page.getByLabel('メールアドレス').fill(testmail);
+        await page.getByLabel('パスワード').fill("aaa");
+        await page.getByRole('button', { name: 'ログイン' }).click();
 
-// 条件:パスワードが間違ってる
-// 期待:｢このフィールドを入力してください｣と表示される
-test('ログイン失敗:パスワードが間違ってる',async({page}) => {
-    await page.goto(baseHome + 'login');
-    await page.getByLabel('メールアドレス').fill(testmail);
-    await page.getByLabel('パスワード').fill("aaa");
-    await page.getByRole('button', { name: 'ログイン' }).click();
+        await expect(page.getByText('Whoops! Something went wrong.')).toBeVisible();
+        await expect(page.getByText('These credentials do not match our records.')).toBeVisible();
+    })
 
-    await expect(page.getByText('Whoops! Something went wrong.')).toBeVisible();
-    await expect(page.getByText('These credentials do not match our records.')).toBeVisible();
-})
+    // 条件:パスワードが間違ってる
+    // 期待:｢このフィールドを入力してください｣と表示される
+    test('メアドが間違ってる',async({page}) => {
+        await page.goto(baseHome + 'login');
+        await page.getByLabel('メールアドレス').fill("abc@abc.com");
+        await page.getByLabel('パスワード').fill(testpass);
+        await page.getByRole('button', { name: 'ログイン' }).click();
 
-// 条件:パスワードが間違ってる
-// 期待:｢このフィールドを入力してください｣と表示される
-test('ログイン失敗:メアドが間違ってる',async({page}) => {
-    await page.goto(baseHome + 'login');
-    await page.getByLabel('メールアドレス').fill("abc@abc.com");
-    await page.getByLabel('パスワード').fill(testpass);
-    await page.getByRole('button', { name: 'ログイン' }).click();
+        await expect(page.getByText('Whoops! Something went wrong.')).toBeVisible();
+        await expect(page.getByText('These credentials do not match our records.')).toBeVisible();
+    })
 
-    await expect(page.getByText('Whoops! Something went wrong.')).toBeVisible();
-    await expect(page.getByText('These credentials do not match our records.')).toBeVisible();
 })
 
 // 条件:ログイン成功
--- e2e/wellcom/wellcom.spec.js
+++ e2e/wellcom/wellcom.spec.js
@@ -23,66 +23,69 @@ test('default', async ({ page }) => {
     // await expect(page.getByRole('link', { name: '問い合わせ' })).toBeVisible();
 })
 
-// 画面遷移系
-// 条件:ログインボタンを押す
-// 期待:ログイン画面が表示される
-test('login button', async ({ page }) => {
-    await page.goto(baseHome);
-    await page.getByRole('button', { name: 'ログイン' }).click();
-    await expect(page).toHaveURL(baseHome + "login");
+test.describe("画面遷移",() => {
+    // 条件:ログインボタンを押す
+    // 期待:ログイン画面が表示される
+    test('ログインボタンを押す', async ({ page }) => {
+        await page.goto(baseHome);
+        await page.getByRole('button', { name: 'ログイン' }).click();
+        await expect(page).toHaveURL(baseHome + "login");
+    })
+
+    // 条件:初めての方はこちらボタンを押す
+    // 期待:新規登録画面が表示される
+    test('初めての方はこちらボタンを押す', async ({ page }) => {
+        await page.goto(baseHome);
+        await page.getByRole('button', { name: '初めての方はこちら' }).click();
+        await expect(page).toHaveURL(baseHome + "register");
+    })
+
+    // 条件:ログインリンクを押す
+    // 期待:ログイン画面が表示される
+    test('ログインリンクを押す', async ({ page }) => {
+        await page.goto(baseHome);
+        await page.locator('a').filter({ hasText: /^ログイン$/ }).click();
+        await expect(page).toHaveURL(baseHome + "login");
+    })
+
+    // 条件:初めての方はこちらリンクを押す
+    // 期待:新規登録画面が表示される
+    test('初めての方はこちらリンクを押す', async ({ page }) => {
+        await page.goto(baseHome);
+        await page.getByRole('button', { name: '初めての方はこちら' }).click();
+        await expect(page).toHaveURL(baseHome + "register");
+    })
+
+    // 条件:英語版リンクを押す
+    // 期待:英語版が表示されているか
+    test('英語版リンクを押す', async ({ page }) => {
+        await page.goto(baseHome);
+        await page.getByRole('link', { name: 'English' }).click();
+
+        await expect(page).toHaveURL(baseHome + "en");
+
+        await expect(page.getByRole('link', { name: '日本語' })).toBeVisible();
+        await expect(page.locator('a').filter({ hasText: /^Log in$/ })).toBeVisible();
+        await expect(page.locator('a').filter({ hasText: /^Click here for the first time$/ })).toBeVisible();
+
+        await expect(page.getByRole('button', { name: 'Log in' })).toBeVisible();
+        await expect(page.getByRole('button', { name: 'Click here for the first time' })).toBeVisible();
+        await page.screenshot({ path: 'playwright-screenshot/wellcome/english.jpg', fullPage: false });
+
+    })
+
+    // 条件:アドオンリンクを押す
+    // 期待:アドオンが表示されているか
+    test('アドオンリンクを押す', async ({ page }) => {
+        await page.goto(baseHome);
+        const page1Promise = page.waitForEvent('popup');
+        await page.getByRole('link', { name: 'ダウンロードはこちらから' }).click();
+        const addonPage = await page1Promise;
+        await expect(addonPage).toHaveURL("https://chrome.google.com/webstore/detail/sundlf-bookmark-addon/mfcobcdpjbgnpbkhbbfaabkkphpceoka");
+    })
+
 })
 
-// 条件:初めての方はこちらボタンを押す
-// 期待:新規登録画面が表示される
-test('register button', async ({ page }) => {
-    await page.goto(baseHome);
-    await page.getByRole('button', { name: '初めての方はこちら' }).click();
-    await expect(page).toHaveURL(baseHome + "register");
-})
-
-// 条件:ログインリンクを押す
-// 期待:ログイン画面が表示される
-test('login link', async ({ page }) => {
-    await page.goto(baseHome);
-    await page.locator('a').filter({ hasText: /^ログイン$/ }).click();
-    await expect(page).toHaveURL(baseHome + "login");
-})
-
-// 条件:初めての方はこちらリンクを押す
-// 期待:新規登録画面が表示される
-test('register link', async ({ page }) => {
-    await page.goto(baseHome);
-    await page.getByRole('button', { name: '初めての方はこちら' }).click();
-    await expect(page).toHaveURL(baseHome + "register");
-})
-
-// 条件:英語版リンクを押す
-// 期待:英語版が表示されているか
-test('english link', async ({ page }) => {
-    await page.goto(baseHome);
-    await page.getByRole('link', { name: 'English' }).click();
-
-    await expect(page).toHaveURL(baseHome + "en");
-
-    await expect(page.getByRole('link', { name: '日本語' })).toBeVisible();
-    await expect(page.locator('a').filter({ hasText: /^Log in$/ })).toBeVisible();
-    await expect(page.locator('a').filter({ hasText: /^Click here for the first time$/ })).toBeVisible();
-
-    await expect(page.getByRole('button', { name: 'Log in' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Click here for the first time' })).toBeVisible();
-    await page.screenshot({ path: 'playwright-screenshot/wellcome/english.jpg', fullPage: false });
-
-})
-
-// 条件:アドオンリンクを押す
-// 期待:アドオンが表示されているか
-test('addon link', async ({ page }) => {
-    await page.goto(baseHome);
-    const page1Promise = page.waitForEvent('popup');
-    await page.getByRole('link', { name: 'ダウンロードはこちらから' }).click();
-    const addonPage = await page1Promise;
-    await expect(addonPage).toHaveURL("https://chrome.google.com/webstore/detail/sundlf-bookmark-addon/mfcobcdpjbgnpbkhbbfaabkkphpceoka");
-})
 
 // 条件:ログインした状態
 // 期待:ログインボタンが非表示
